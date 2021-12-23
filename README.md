046fd2a639d7df996a221c816eda377799ba6897
joncrall <jon.crall@kitware.com>
2021-12-23 14:58:53 -0500

    Update README / add script

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # PYthon version of COntinuous monitoring of Land Disturbance algorithm (V0.1)
 ####  History
 - Version 0.2: add pip-installable functionality by using scikit-build
-- Version 0.1: a Python package based on C library of COLD. The verification with MATLAB COLD shows that  C-COLD has smaller than <2% differences for breakpoint detection and <2% differences for harmonic coefficents
+- Version 0.1: a Python package based on C library of COLD. The verification with MATLAB COLD shows that  C-COLD has smaller than <2% differences for breakpoint detection and <2% differences for harmonic coefficients
 
 ## 1. Pre-work: clone github repo to your local directory
 ### Clone or pull the latest pycold git repo
@@ -12,30 +12,49 @@ git clone https://gitlab.kitware.com/smart/pycold.git
 ```
 
 ## 2. installation
+
+The steps to install this library in development mode are consolidated into a
+single script: `run_developer_setup.sh`. The following is an overview of these
+details and alternative choices that could be made.
+
 ### 2.1 install required libraries
-sudo apt-get install build-essential zlib1g-dev
-sudo apt-get install libgsl-dev
-sudo apt-get install gfortran
+
+The ZLIB, GSL, and OpenMP libraries are required.
+
+For Ubuntu/Debian systems, they can be installed via:
+
+```bash
+sudo apt-get install build-essential zlib1g-dev -y
+sudo apt-get install libgsl-dev -y
+sudo apt-get install gfortran -y
+```
 
 ### 2.2 install pycold
 **Option 1: Build and install a wheel** 
-(Scikit-build will invoke CMake and build everythinig)
-```
+
+Scikit-build will invoke CMake and build everything. (you may need to remove
+any existing `_skbuild` directory).
+
+```bash
 python setup.py bdist_wheel
 ```
+
 Then you can pip install the wheel
-```
+
+```bash
 pip install dist/pycold-0.1.0-cp38-cp38-linux_x86_64.whl
 ```
 
 **Option 2: Install in development mode**
-```
+
+```bash
 pip install -r requirements.txt
 pip install -e .
 ```
 
 **Option 3: build standalone binaries with CMake by itself (recommended for C development)**
-```
+
+```bash
 mkdir -p build
 cd build
 cmake ..
---
+++ run_developer_setup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+__doc__="
+Script that does its best to ensure a robust installation of pycold in
+development mode.
+"
+
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+    echo "WARNING: NOT INSIDE OF A Python VIRTUAL_ENV. This script may not run correctly"
+fi 
+
+apt_ensure(){
+    __doc__="
+    Checks to see if the packages are installed and installs them if needed.
+
+    The main reason to use this over normal apt install is that it avoids sudo
+    if we already have all requested packages.
+
+    Args:
+        *ARGS : one or more requested packages 
+
+    Example:
+        apt_ensure git curl htop 
+
+    Ignore:
+        REQUESTED_PKGS=(git curl htop) 
+    "
+    # Note the $@ is not actually an array, but we can convert it to one
+    # https://linuxize.com/post/bash-functions/#passing-arguments-to-bash-functions
+    ARGS=("$@")
+    MISS_PKGS=()
+    HIT_PKGS=()
+    for PKG_NAME in "${ARGS[@]}"
+    do
+        #apt_ensure_single $EXE_NAME
+        RESULT=$(dpkg -l "$PKG_NAME" | grep "^ii *$PKG_NAME")
+        if [ "$RESULT" == "" ]; then 
+            echo "Do not have PKG_NAME='$PKG_NAME'"
+            MISS_PKGS=(${MISS_PKGS[@]} "$PKG_NAME")
+        else
+            echo "Already have PKG_NAME='$PKG_NAME'"
+            HIT_PKGS=(${HIT_PKGS[@]} "$PKG_NAME")
+        fi
+    done
+
+    if [ "${#MISS_PKGS}" -gt 0 ]; then
+        sudo apt install -y "${MISS_PKGS[@]}"
+    else
+        echo "No missing packages"
+    fi
+}
+
+
+# If on debian/ubuntu ensure the dependencies are installed 
+if [[ "$(command -v apt)" != "" ]]; then
+    apt_ensure build-essential zlib1g-dev libgsl-dev gfortran
+else
+    echo "
+    WARNING: Check and install of system packages is currently only supported
+    on Debian Linux. You will need to verify that ZLIB, GSL, OpenMP are
+    installed before running this script.
+    "
+fi
+
+
+if [ -d "./_skbuild" ]; then
+    # Cleanup the scikit build directory
+    echo "Removing old _skbuild directory"
+    rm -rf _skbuild
+else
+    echo "Detected clean state (no _skbuild dir)"
+fi
+
+# Compile and install PyCold in development mode.
+pip install --verbose -e . 
+
+
+# Quick tests that the install worked
+echo "Pycold Version: $(python -c 'import pycold; print(pycold.__version__)')"
+echo "Python Package Location: $(python -c 'import pycold; print(pycold.__file__)')"
+echo "Compiled Cython Module: $(python -c 'import pycold; print(pycold._pycold_cython.__file__)')"
