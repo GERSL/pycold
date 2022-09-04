2ded3205318b00a9294084d42326aeab63186927
joncrall <jon.crall@kitware.com>
2022-09-03 20:19:33 -0400

    Fix CI errors

--- src/python/pycold/__init__.py
+++ src/python/pycold/__init__.py
@@ -3,4 +3,8 @@ __version__ = '0.1.0'
 # Ensure monkey patches run first
 from . import monkey  # NOQA
 
-from ._pycold_cython import *  # NOQA
+from . import _pycold_cython  # NOQA
+
+from ._pycold_cython import (  # NOQA
+    cold, obcold_reconstruction_procedure, sccd, test_func, cold_detect,
+    obcold_reconstruct, sccd_detect, sccd_update)
